9f7b9f06aeaa9d5e486934fc456c15b168210433
AytacAmrahova <aytajamrahova@gmail.com>
2021-05-09 11:38:17 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+var full_name , phone  , email, university, major;
+
+full_name = "Aytac Amrahova";
+phone = "0506878877";
+email="aytajamrahova@gmail.com";
+university = "Khazar University";
+major= "Computer Science";
+
+console.log(full_name);
+console.log(phone);
+console.log(email);
+console.log(university);
+console.log(major);
+
+{
+    let full_name , phone  , email, university, major;
+    full_name = "Nezrin Suleymanova";
+    phone = "0508997889";
+    email="nazrin.suleymanova@gmail.com";
+    university = "Khazar University";
+    major= "Computer Science";
+    console.log(full_name);
+    console.log(phone);
+    console.log(email);
+    console.log(university);
+    console.log(major);
+}
+
+const student_id = "B.01.509.19.008",department = "School of Science and Engineering", group = "A" , GPA = "91"
+
+    console.log(student_id);
+    console.log(department);
+    console.log(group);
+    console.log(GPA);
